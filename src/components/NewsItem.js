720924554a301b63d9a67e684b38fda97e400eca
RahulMangukiya9054 <rahulmangukiya1@gmail.com>
2024-04-24 13:05:00 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+    return (
+      <div className='my-3'>
+        <div className="card">
+
+          <div style={{display:'flex', justifyContent:'flex-end', position:'absolute', right:'0'}}>
+            <span className="badge bg-danger">{props.source}</span>
+          </div>
+
+          <img src={props.imageUrl?props.imageUrl:"https://images.cnbctv18.com/wp-content/uploads/2023/08/Mobile-internet-1019x573.jpg"} className="card-img-top" alt="Sorry, Image Not Available"/>
+          <div className="card-body">
+              <h5 className="card-title">{props.title}</h5>
+              <p className="card-text">{props.description}</p>
+              <p className="card-text"><small className="text-success">By {props.author?props.author:"Unknown"} on {new Date(props.date).toGMTString()}</small></p>
+              <a href={props.newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default NewsItem
